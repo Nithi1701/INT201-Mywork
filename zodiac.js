bb0d4a12806afd4ccdac8bc576fb58780e3a1c4f
Nithi1701 <ballnithilampay1@gmail.com>
2021-08-26 17:57:55 +0700

    Zodiac

---
+++ zodiac.js
@@ -0,0 +1,5 @@
+let zodiac = ['monkey', 'rooster', 'dog', 'pig',
+             'rat', 'ox', 'tiger', 'rabbit', 
+             'dragon', 'snake', 'horse', 'sheep'];
+let year = 1900;
+console.log(zodiac[year % 12])
